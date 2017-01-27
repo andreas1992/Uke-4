42dfed358f949666383f72b254ee7babc13846e6
Andreas <aks1992@gmail.com>
2017-01-27 19:32:33 +0100

    initial commit

---
+++ SomeName.java
@@ -0,0 +1,11 @@
+public class SomeName {
+
+  public static void main(String[] args) {
+    SomeName someName = new SomeName();
+    someName.printSomething("hei");
+  }
+
+  public void printSomething(String something) {
+    System.out.println(something);
+  }
+}
